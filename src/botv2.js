c4b7fd38d6a40280f2cd0f180d96bf7fe0af64a9
Joshua Malek <36347275+17jmalek98@users.noreply.github.com>
2019-12-26 23:05:48 +0300

    Update botv2.js
    added comments

--- src/botv2.js
+++ src/botv2.js
@@ -15,7 +15,7 @@ const manager = new TradeOfferManager({
   language: 'en'
 
 });
-
+//RETRIEVE CREDENTIALS FROM CONFIG
 var outcome = "";
 var value = "";
 client.logOn({
@@ -33,58 +33,59 @@ value = client.steamID.isValid();
   {
     outcome = "VALID";
   }
-  else{
+  else
+  {
     outcome = "INVALID";
   }
   console.log("Profile is " + outcome);
   client.setPersona(SteamUser.EPersonaState.Online);
   client.gamesPlayed(308040);
-
 })
 
 
 //ADDING A FRIEND AND FIRST CHAT
 client.on('friendRelationship', (steamid, relationship) => {
     if (relationship === 2) {
-
         var name = "";
         client.addFriend(steamid);
         client.chatMessage(steamid, 'Hello there! Thanks for adding me! Type !start to continue.');
         console.log(">New Event: Friend [" + steamid + "] added");
-
     }
 });
 
 //CHATTING WITH CUSTOMERS
 client.on("friendMessage", function(steamID, message){
   var id = "";
-  if(steamID == config.ownerID){
+  if(steamID == config.ownerID)
+  {
     id = "ADMIN";
   }
-  else if(steamID == config.yangID){
+  else if(steamID == config.yangID)
+  {
     id = "YANG";
   }
-  else{
+  else
+  {
     id = "CUSTOMER";
   }
-  if(message == "!commands"){
+  if(message == "!commands")
+  {
     console.log("[" + steamID + "] says: " + message.bold);
     console.log("Available commands:\n !id \n !buy".reset);
     client.chatMessage(steamID, "Available commands:\n !id \n !buy");
   }
-  else if(message == "!start"){
+  else if(message == "!start")
+  {
     console.log("[" + steamID + "] says: " + message.bold);
     console.log("Welcome to Keys2Cards 0.0.3.  Enter '!commands' to view available commands.".reset);
     client.chatMessage(steamID, "Hello, and welcome to Keys2Cards 0.0.3.  Enter '!commands' to view available commands.");
   }
-  else if(message == "!id"){
-
+  else if(message == "!id")
+  {
     console.log("[" + steamID + "] says: " + message.bold);
     console.log("You are a ".reset + id);
     client.chatMessage(steamID, "You are a " + id);
   }
-
-
   else{
     console.log("[" + steamID + "] says: " + message.bold);
     console.log("Type '!start' to continue".reset);
@@ -93,15 +94,14 @@ client.on("friendMessage", function(steamID, message){
 });
 
 
-
-
+//SET COOKIES
 client.on('webSession', (sessionid, cookies) => {
     manager.setCookies(cookies);
 
     community.setCookies(cookies);
     community.startConfirmationChecker(10000, config.identitySecret);
 });
-
+//ACCEPT OFFER HELPER 
 function acceptOffer(offer) {
     offer.accept((err) => {
         community.checkConfirmations();
@@ -109,26 +109,36 @@ function acceptOffer(offer) {
         if (err) console.log("There was an error accepting the offer.");
     });
 }
-
+//DECLINE OFFER HELPER 
 function declineOffer(offer) {
     offer.decline((err) => {
         console.log("We Declined an offer");
         if (err) console.log("There was an error declining the offer.");
     });
 }
-
+//PROCESS OFFER HELPER
 function processOffer(offer) {
-    if (offer.isGlitched() || offer.state === 11) {
+    //Make sure offer is not glitched
+    if (offer.isGlitched() || offer.state === 11) 
+    {
         console.log("Offer was glitched, declining.");
         declineOffer(offer);
-    } else if (offer.partner.getSteamID64() == config.ownerID) {
+    } 
+    //Check if offer account is admin 
+    else if (offer.partner.getSteamID64() == config.ownerID) 
+    {
+        //always accept offer from admin
         console.log("Offer is from admin")
         acceptOffer(offer);
-    } else {
+    } 
+    else 
+    {
+        //calculate value of offer items vs. what we trade
         var ourItems = offer.itemsToGive;
         var theirItems = offer.itemsToReceive;
         var ourValue = 0;
         var theirValue = 0;
+        //scan through market listings to retrieve market prices for our items
         for (var i in ourItems) {
             var item = ourItems[i].market_name;
             if(Prices[item]) {
@@ -138,6 +148,7 @@ function processOffer(offer) {
                 ourValue += 99999;
             }
         }
+        //scan through market listings to retrieve market prices for their items
         for(var i in theirItems) {
             var item= theirItems[i].market_name;
             if(Prices[item]) {
@@ -147,18 +158,19 @@ function processOffer(offer) {
             }
         }
     }
+    //print our values vs theirs
     console.log("Our value: " + ourValue);
     console.log("Their value: " + theirValue);
-
+    //If our offer is worth less than theirs, accept
     if (ourValue <= theirValue) {
         acceptOffer(offer);
     } else {
         declineOffer(offer);
     }
 }
-
+//remove need for steam guard code
 client.setOption("promptSteamGuardCode", false);
-
+//set manager to async run
 manager.on('newOffer', (offer) => {
      processOffer(offer);
 });
